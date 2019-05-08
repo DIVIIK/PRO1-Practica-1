4af3463271a660ebe761c0711f81e4a50790400f
Oriol Fernández <theoriolfb@gmail.com>
2019-05-08 20:06:21 +0200

    Mejora general y en es_pot_despl

--- escaquer.cc
+++ escaquer.cc
@@ -9,9 +9,10 @@
 //---- Constructora. Serveix per declarar una variable
 //---- de tipus "escaquer" de mida n x n inicialitzada
 //---- amb les peces inicials d'una partida de Dames.
+
+/* PRE: n és parell i n>=8 */
+/* POST: crea un vector de vectors de caselles (Una taula de caselles) de n files i n columnes */
 escaquer::escaquer(nat n) {
-  /* PRE: n és parell i n>=8 */
-  /* POST: crea un vector de vectors de caselles (Una taula de caselles) de n files i n columnes */
   taula = vector<vector<casella> >(n,vector<casella>(n,casella()));
   // IMPORTANT!!! coord(x,y) x=files y=columnes
 
@@ -39,14 +40,16 @@ escaquer::escaquer(nat n) {
 }
 
 //---- Diu si les coord p estan dins dels limits del escaquer.
+
+/* PRE: p es una coordenada (x,y), p = P */
+/* POST: Retorna cert si P <= taula.size(), es a dir que P es troba dintre del escaquer, d'altre forma retorna fals */
 bool escaquer::dins_limits(coord p) const {
-  /* PRE: p es una coordenada (x,y), p = P */
-  /* POST: Retorna cert si P <= taula.size(), es a dir que P es troba dintre del escaquer, d'altre forma retorna fals */
   if ( (p.x >= 0 and p.x < taula.size()) and (p.y >= 0 and p.y < taula.size()) )  return true;
   else return false;
 }
 
 //---- Accedeix a la casella de la coord p del escaquer.
+
   /* PRE: p es una coordenada (x,y), p = P */
   /* POST: Retorna la casella de l'escaquer corresponent a la coordenada P */
 casella& escaquer::operator()(coord p) {
@@ -75,9 +78,10 @@ void escaquer::mostra() const {
 
 //---- Escriu a la pantalla el contingut del escaquer marcant amb '?'
 //---- les caselles on es poden posar fitxes del color donat.
+
+/* PRE: color indica l'equip del cual mostrem els possibles moviments, -3 < color < 4 */
+// Fem una pasada per guardar les coordenades de dames amb el color especificat.
 void escaquer::mostra(int color) const {
-  /* PRE: color indica l'equip del cual mostrem els possibles moviments, -3 < color < 4 */
-  // Fem una pasada per guardar les coordenades de dames amb el color especificat.
   vector<coord> coords;
   for (int x = 0; x < taula.size(); ++x)
     for (int y = 0; y < taula.size(); ++y)
@@ -115,8 +119,9 @@ void escaquer::mostra(int color) const {
 
 //---- Avalua les fitxes del escaquer:
 //---- num_caselles_blanques - num_caselles_negres 
+
+/* POST: Retorna el resultat de restar la cuantitat de peçes blanques i negres que queden al escaquer */
 int escaquer::avalua() const {
-  /* POST: Retorna el resultat de restar la cuantitat de peçes blanques i negres que queden al escaquer */
   int b = 0;
   int n = 0;
 
@@ -130,9 +135,10 @@ int escaquer::avalua() const {
 
 //---- Comprova si es pot despl desde la coordenada cini en la direcció d
 //---- Retorna: despl (si es pot despl o no), c (coordenada final després del moviment)
+
+/* PRE: cini son les coordenades inicials, d es la direccio on es mou la peça, */
+/*      despl indica si pot fer el moviment o no, c son les coordenades finals resultants */ 
 void escaquer::es_pot_despl(coord cini, direccio d, bool &despl, coord &c) const {
-  /* PRE: cini son les coordenades inicials, d es la direccio on es mou la peça, */
-  /*      despl indica si pot fer el moviment o no, c son les coordenades finals resultants */ 
   despl = true;
   c = (cini + d.despl());
   int valorIni = taula[cini.x][cini.y].valor();
@@ -144,35 +150,27 @@ void escaquer::es_pot_despl(coord cini, direccio d, bool &despl, coord &c) const
   } else {
     int valorDespl = taula[c.x][c.y].valor();
 
-    // Comprovacions per moviment si es negra
-    if ((valorIni == casella::NEGRA or valorIni == casella::DAMA_NEGRA)) {
-      
-      // Mirar si nomes es negra va cap abaix
-      if (valorIni == casella::NEGRA)
-        if (d.mostra() == "NORD-EST" or d.mostra() == "NORD-OEST") despl = false; // Amb (direccio::NO | direccio::NE) no funciona be
-            
-      // Comprovar si hi ha una fixa al desplaçament
-      if (valorDespl != casella::LLIURE) es_pot_capturar(cini,d,despl,c);
-    }
+    // Comprovar si hi ha una fixa al desplaçament
+    if (valorDespl != casella::LLIURE) {
+      despl = false;
+    
+    // Comprovacions negra
+    } else if ( (valorIni == casella::NEGRA) and (d.mostra() == "NORD-EST" or d.mostra() == "NORD-OEST") ) {
+      despl = false;
+    
+    // Comprovacions blanca            
+    } else if ( (valorIni == casella::BLANCA) and (d.mostra() == "SUD-EST" or d.mostra() == "SUD-OEST") )
+      despl = false;
 
-    // Comprovacions per moviment si es blanca
-    if ((valorIni == casella::BLANCA or valorIni == casella::DAMA_BLANCA)) {
-      
-      // Mirar si nomes es blanca va cap amunt
-      if (valorIni == casella::BLANCA)
-        if (d.mostra() == "SUD-EST" or d.mostra() == "SUD-OEST") despl = false; // Amb (direccio::SO | direccio::SE) no funciona be
-            
-      // Comprovar si hi ha una fixa al desplaçament
-      if (valorDespl != casella::LLIURE) es_pot_capturar(cini,d,despl,c);
-    }
   }
 }
 
 //---- Comprova si es pot capturar desde la coordenada cini en la direcció d
 //---- Retorna: capturar (si es pot capturar o no), c (coordenada final després de la captura)
+
+/* PRE: cini son les coordenades inicials, d es la direccio on es mou la peça, */
+/*      capturar indica si pot fer la captura o no, c son les coordenades finals resultants */ 
 void escaquer::es_pot_capturar(coord cini, direccio d, bool &capturar, coord &c) const {
-  /* PRE: cini son les coordenades inicials, d es la direccio on es mou la peça, */
-  /*      capturar indica si pot fer la captura o no, c son les coordenades finals resultants */ 
 
   // valorIni     es la peça a moure
   // valorDespl   es la peça a capturar
@@ -218,11 +216,10 @@ void escaquer::es_pot_capturar(coord cini, direccio d, bool &capturar, coord &c)
 //---- Cal comprovar si en una de les 4 direccions hi ha una peça dama del color
 //---- o bé en los 2 direccions una peça del color
 //---- també es poden capturar fitxes de l'adversari (de color diferent al color donat).
-list<coord> escaquer::mov_possibles(coord c) const {
-  /* PRE: c son les coordenades de la peça a evaluar, c = C */
-  /* POST: Retorna una llista amb totes les coordenades posibles on es pot moure C */
 
-  // Tots els moviments possibles
+/* PRE: c son les coordenades de la peça a evaluar, c = C */
+/* POST: Retorna una llista amb totes les coordenades posibles on es pot moure C */
+list<coord> escaquer::mov_possibles(coord c) const {
   list<coord> coords;
   direccio dir;
   dir.init();
@@ -241,9 +238,10 @@ list<coord> escaquer::mov_possibles(coord c) const {
 
 
 //---- Comprova si el color pot jugar (la peça de color es pot col·locar en algun lloc).
+  
+/* PRE: color indica l'equip que estem mirant si pot jugar, -3 < color < 4 */
+/* POST: Retorna cert si el color pot fer algun moviment amb les fixes del escaquer, si hi han. D'altre forma retorna fals */
 bool escaquer::pot_jugar(int color) const {
-  /* PRE: color indica l'equip que estem mirant si pot jugar, -3 < color < 4 */
-  /* POST: Retorna cert si el color pot fer algun moviment amb les fixes del escaquer, si hi han. D'altre forma retorna fals */
   bool potJugar = false;
   int x = 0;
   int y = 0;
@@ -298,6 +296,9 @@ bool escaquer::posa_fitxa(coord c, coord cf, int color) {
 
   return esPot;
 }
+
+//________________________________________//
+//////////// Metodes privats //////////////
 
 /* PRE: p son les coordenades on actualment es la fixa, color es el tipus de fixa */
 void escaquer::convertir_dama(coord &p, int &color) {
